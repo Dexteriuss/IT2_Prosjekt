5e8f3486d184e6d6156076f5737ecb31f86276b8
Dexteriuss <159881820+Dexteriuss@users.noreply.github.com>
2024-02-27 09:41:40 +0100

    Implementert_taster
    Har implementert tastaturknapper i spillet, slik at man kan styre Mario-figuren slik man ønsker

--- Hoppe_mario.py
+++ Hoppe_mario.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from pygame.locals import (K_UP, K_DOWN, K_LEFT, K_RIGHT)
 
 pygame.init()
 
@@ -8,7 +9,6 @@ SKJERM = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("Hoppe i Pygame")
 
 X_POSISJON, Y_POSISJON = 400, 650
-
 hopping = False
 
 Y_GRAVITASJON = 1
@@ -20,6 +20,22 @@ BACKGROUND = pygame.transform.scale(pygame.image.load("WHu9Z.png"), (800, 800))
 
 mario_rect = STANDING_SURFACE.get_rect(center=(X_POSISJON, Y_POSISJON))
 
+class Mario:
+    """Klasse for å representere en Mario figur"""
+    def __init__(self, x, y, fart, vindusobjekt):
+        self.x = x
+        self.y = y
+        self.fart = fart
+        self.vindusobjekt = vindusobjekt
+    
+    def flytt(self, taster):
+        if taster[K_LEFT]:
+            self.x -= self.fart
+        if taster[K_RIGHT]:
+            self.x += self.fart
+
+mario = Mario(X_POSISJON, Y_POSISJON, 5, SKJERM)  # Opprett et Mario-objekt
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -30,6 +46,9 @@ while True:
     
     if keys_pressed[pygame.K_SPACE]:
         hopping = True
+
+    # Kall flytt-metoden for Mario-objektet basert på tastetrykkene
+    mario.flytt(keys_pressed)
     
     SKJERM.blit(BACKGROUND, (0, 0))
     
@@ -39,14 +58,13 @@ while True:
         if Y_VELOCITY < -HOPPE_HØYDE:
             hopping = False
             Y_VELOCITY = HOPPE_HØYDE
-        mario_rect = JUMPING_SURFACE.get_rect(center=(X_POSISJON, Y_POSISJON))
+        mario_rect = JUMPING_SURFACE.get_rect(center=(mario.x, Y_POSISJON))
         SKJERM.blit(JUMPING_SURFACE, mario_rect)
     
     else:
-        mario_rect = STANDING_SURFACE.get_rect(center=(X_POSISJON, Y_POSISJON))
+        mario_rect = STANDING_SURFACE.get_rect(center=(mario.x, Y_POSISJON))
         SKJERM.blit(STANDING_SURFACE, mario_rect)
-    
     
     pygame.display.update()
     KLOKKE.tick(60)
-    
+ 
